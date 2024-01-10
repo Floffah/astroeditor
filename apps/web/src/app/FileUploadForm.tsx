b1132284fbcdd0df56d4b1e9861360a7e0bec0e7
Floffah <floffah@floffah.dev>
2024-01-10 15:16:27 +0000

    fix: edit upload form comment

--- apps/web/src/app/FileUploadForm.tsx
+++ apps/web/src/app/FileUploadForm.tsx
@@ -39,18 +39,24 @@ export function FileUploadForm() {
 
 			<div className="bg-white/20 w-full h-0.5" />
 
-			<div>
+			<div className="flex flex-col gap-2">
 				<p>
 					<strong>DISCLAIMER: </strong>
 					This is a fan-made tool and is not affiliated with or endorsed by the
 					Astroneer team. To the respect of the Astroneer team, all save files
-					will be set to creative mode with achievements disabled. So no one can
-					get around this restriction, this tool is also <strong>
-						NOT
-					</strong>{' '}
-					open source and I have no intention of making it open source unless
-					requested by the Astroneer dev team.
+					will be set to creative mode with achievements disabled.
 				</p>
+				<p>
+					This tool is <strong>VERY WIP</strong> and should not be expected to
+					work correctly. I don&apos;t have a lot of time to work on this, so
+					expect slow updates. Follow updates on the GitHub repository
+				</p>
+				<a
+					href="https://github.com/floffah/astroeditor"
+					className="text-blue-300 hover:decoration-1 hover:underline"
+				>
+					GitHub repository
+				</a>
 			</div>
 		</div>
 	)
